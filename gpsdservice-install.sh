10aa380825f6fd46b8a51a7acab297c9be670b7f
thespinmaster <nealandrews@outlook.com>
2025-03-31 10:23:11 +0000

    update gpsdservice-install.sh

--- gpsdservice-install.sh
+++ gpsdservice-install.sh
@@ -1,15 +1,15 @@
-#!/bin/sh
+#!/bin/bash
  
 echo Enter the IP address of the Venus OS
 read ip
 
-source="https://raw.githubusercontent.com/thespinmaster/GLX3000-GPS/refs/heads/main/"
+source="https://raw.githubusercontent.com/thespinmaster/GLX3000-GPS/refs/heads/main"
 
 #download gsddservice
-wget -O /etc/init.d/gpsdservice $source + "gpsdservice"
+wget -O "/etc/init.d/gpsdservice" "${source}/gpsdservice"
 
 #download service config
-wget -O /etc/config/gpsdservice $source + "gpsdservice-config"
+wget -O "/etc/config/gpsdservice""" "${source}/gpsdservice-config"
 
 #change the permissions for the service
 chmod 755 /etc/init.d/gpsdservice
@@ -19,6 +19,6 @@ uci set gpsdservice.venus.ip=$ip
 
 #enable and start the service
 /etc/init.d/gpsdservice enable
-/etc/init.d/gpsdservice start
+# /etc/init.d/gpsdservice start
 
 
