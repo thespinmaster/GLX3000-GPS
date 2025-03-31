d229aca8852bedd2e30f4d89eaa4c92810169935
thespinmaster <nealandrews@outlook.com>
2025-03-31 16:40:31 +0000

    update gpsdservice-install.sh

--- gpsdservice-install.sh
+++ gpsdservice-install.sh
@@ -14,7 +14,7 @@ source="https://raw.githubusercontent.com/thespinmaster/GLX3000-GPS/refs/heads/m
 wget -O "/etc/init.d/gpsdservice" "${source}/gpsdservice"
 
 #download service config
-wget -O "/etc/config/gpsdservice""" "${source}/gpsdservice-config"
+wget -O "/etc/config/gpsdservice" "${source}/gpsdservice-config"
 
 echo change the permissions for the service
 chmod 755 /etc/init.d/gpsdservice
